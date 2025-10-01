3bf91eb9e53f532d57fe883aa8b8fc224628882e
Elvis3881 <elvisjoelalexander@gmail.com>
2025-10-01 17:04:18 -0500

    Add files via upload
    Entidad Rol agregada

--- src/main/java/com/example/grupod_atencionpersonasmayoressise/api/ApiRol.java
+++ src/main/java/com/example/grupod_atencionpersonasmayoressise/api/ApiRol.java
@@ -1,33 +1,45 @@
 package com.example.grupod_atencionpersonasmayoressise.api;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import com.example.grupod_atencionpersonasmayoressise.iservices.IRolService;
 import com.example.grupod_atencionpersonasmayoressise.model.Rol;
-import com.example.grupod_atencionpersonasmayoressise.services.RolService;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
+
 
 @RestController
 @RequestMapping("back/rol")
 public class ApiRol {
     @Autowired
-    RolService rolService;
+    IRolService iRolService;
 
 
     @GetMapping("/listarRol")
     public List<Rol>listar() {
-        return  rolService.listar();
+        return  iRolService.listar();
     }
 
     @GetMapping("/obtenerRol")
-    public Rol obtenerPorId(@PathVariable Long id) {
-        return rolService.obtenerPorId(id);
+    public Rol obtenerPorId(@RequestParam Long id) {
+        return iRolService.obtenerPorId(id);
     }
-    
+  @GetMapping("/paginado")
+    public Page<Rol> paginado(
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "5") int size,
+        @RequestParam(value = "search", required = false) String search
+    ) {
+        return iRolService.paginado(search, PageRequest.of(page, size));
+    }
     
 }
