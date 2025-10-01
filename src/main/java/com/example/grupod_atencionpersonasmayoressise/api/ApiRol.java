7483c8638b6e1c2d532863364a058293105fe61f
Erick Romero <romerocruzerick11@gmail.com>
2025-10-01 17:55:48 -0500

    Guardando cambios antes de merge/revert

--- src/main/java/com/example/grupod_atencionpersonasmayoressise/api/ApiRol.java
+++ src/main/java/com/example/grupod_atencionpersonasmayoressise/api/ApiRol.java
@@ -11,8 +11,7 @@ import com.example.grupod_atencionpersonasmayoressise.model.Rol;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -33,13 +32,13 @@ public class ApiRol {
     public Rol obtenerPorId(@RequestParam Long id) {
         return iRolService.obtenerPorId(id);
     }
-  @GetMapping("/paginado")
-    public Page<Rol> paginado(
-        @RequestParam(defaultValue = "0") int page,
-        @RequestParam(defaultValue = "5") int size,
-        @RequestParam(value = "search", required = false) String search
-    ) {
-        return iRolService.paginado(search, PageRequest.of(page, size));
-    }
+//   @GetMapping("/paginado")
+//     public Page<Rol> paginado(
+//         @RequestParam(defaultValue = "0") int page,
+//         @RequestParam(defaultValue = "5") int size,
+//         @RequestParam(value = "search", required = false) String search
+//     ) {
+//         return iRolService.paginado(search, PageRequest.of(page, size));
+//     }
     
 }
