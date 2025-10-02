8739f47b16d1707ba4b72a8bc11c035aa30949f6
Elvis3881 <elvisjoelalexander@gmail.com>
2025-10-01 19:22:21 -0500

    .

--- src/main/java/com/example/grupod_atencionpersonasmayoressise/iservices/IRolService.java
+++ src/main/java/com/example/grupod_atencionpersonasmayoressise/iservices/IRolService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import com.example.grupod_atencionpersonasmayoressise.model.Empleado;
 import com.example.grupod_atencionpersonasmayoressise.model.Rol;
 
 public interface IRolService {
@@ -18,5 +17,7 @@ public interface IRolService {
     Rol actualizar(Rol rol);
 
     Rol elimarLogico(Long id);
+      public List<Rol> listarRol();
+
     public Page<Rol> paginado(String search, Pageable pageable);
 }
